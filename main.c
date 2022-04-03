24e62858f29703d7cd6ced080c98e10123aba0c9
PRUXA666 <Stalin41k@yandex.ru>
2022-04-03 13:49:08 +0700

    add if else whith print 'Hello Space'

--- main.c
+++ main.c
@@ -4,6 +4,11 @@ int main() {
 	int i; 
 	scanf("%d", &i);
 	printf("%d\n", i)
-	printf("Hello World");
+		if (i % 2 == 1) {
+			printf("Hello World");
+		}
+		else {
+			printf("Hello Space");
+		}
 	return 0;
 }
